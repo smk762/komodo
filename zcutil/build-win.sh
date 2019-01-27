6dcbca895a09858883c1be354839e9970e01b445
ca333 <ca333@users.noreply.github.com>
2019-01-28 00:56:14 +0100

    add libcc build to win makefile
    windows specific fix

--- zcutil/build-win.sh
+++ zcutil/build-win.sh
@@ -12,6 +12,21 @@ cd "$(dirname "$(readlink -f "$0")")/.."
 cd depends/ && make HOST=$HOST V=1 NO_QT=1
 cd ../
 ./autogen.sh
+#BUILD CCLIB
+
+WD=$PWD
+cd src/cc
+echo $PWD
+
+if make "$@"; then
+  echo CCLIB BUILD SUCCESSFUL
+else
+  echo CCLIB BUILD FAILED
+  exit 1
+fi
+
+cd $WD
+
 CONFIG_SITE=$PWD/depends/x86_64-w64-mingw32/share/config.site CXXFLAGS="-DPTW32_STATIC_LIB -DCURL_STATICLIB -DCURVE_ALT_BN128 -fopenmp -pthread" ./configure --prefix="${PREFIX}" --host=x86_64-w64-mingw32 --enable-static --disable-shared
 sed -i 's/-lboost_system-mt /-lboost_system-mt-s /' configure
 cd src/
