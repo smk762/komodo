299342d9333378858318726416dee9ea9fe363b3
ca333 <ca333@users.noreply.github.com>
2019-11-25 13:17:04 +0100

    update libsodium URL

--- depends/packages/libsodium.mk
+++ depends/packages/libsodium.mk
@@ -1,11 +1,10 @@
 package=libsodium
 $(package)_version=1.0.15
-$(package)_download_path=https://download.libsodium.org/libsodium/releases/old
+$(package)_download_path=https://download.libsodium.org/libsodium/releases/old/unsupported
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=fb6a9e879a2f674592e4328c5d9f79f082405ee4bb05cb6e679b90afe9e178f4
 $(package)_dependencies=
 $(package)_config_opts=
-
 ifeq ($(build_os),darwin)
 define $(package)_set_vars
   $(package)_build_env=MACOSX_DEPLOYMENT_TARGET="10.11"
