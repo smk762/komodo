84332a2faa53febf1119b70ed55586d44472f3c5
dimxy <dimxy@komodoplatform.com>
2021-04-28 15:02:08 +0500

    refactored komodo_nSPVreq code; standard logging added; fixed static nspv json buffer to dynamic

--- src/komodo_nSPV_defs.h
+++ src/komodo_nSPV_defs.h
@@ -110,7 +110,7 @@ struct NSPV_mempoolresp
     char coinaddr[64];
     uint256 txid;
     int32_t nodeheight,vout,vindex;
-    uint16_t numtxids; uint8_t CCflag,funcid;
+    uint16_t numtxids; uint8_t CCflag, funcid;
 };
 
 struct NSPV_ntz
@@ -187,7 +187,7 @@ struct NSPV_CCmtxinfo
 struct NSPV_remoterpcresp
 {
     char method[64];
-    char json[11000];
+    char *json;
 };
 
 #endif // KOMODO_NSPV_DEFSH
