6cbdf4a922e0366456cb34cc892ac48d7374de20
dimxy <dimxy@komodoplatform.com>
2021-04-28 16:01:43 +0500

    fix check for non-zero eval code in make conds funcs

--- src/cc/CCtokenutils.cpp
+++ src/cc/CCtokenutils.cpp
@@ -428,7 +428,8 @@ CC *MakeTokensCCcond1of2(uint8_t evalcode, uint8_t evalcode2, CPubKey pk1, CPubK
     pks.push_back(CCNewSecp256k1(pk2));
 
     std::vector<CC*> thresholds;
-    thresholds.push_back(CCNewEval(E_MARSHAL(ss << evalcode)));
+    if (evalcode != 0)
+        thresholds.push_back(CCNewEval(E_MARSHAL(ss << evalcode)));
     if (evalcode != EVAL_TOKENS)	                                                // if evalCode == EVAL_TOKENS, it is actually MakeCCcond1of2()!
         thresholds.push_back(CCNewEval(E_MARSHAL(ss << (uint8_t)EVAL_TOKENS)));	    // this is eval token cc
     if (evalcode2 != 0)
@@ -449,7 +450,8 @@ CC *MakeTokensCCcond1(uint8_t evalcode, uint8_t evalcode2, CPubKey pk)
     pks.push_back(CCNewSecp256k1(pk));
 
     std::vector<CC*> thresholds;
-    thresholds.push_back(CCNewEval(E_MARSHAL(ss << evalcode)));
+    if (evalcode != 0)
+        thresholds.push_back(CCNewEval(E_MARSHAL(ss << evalcode)));
     if (evalcode != EVAL_TOKENS)                                                    // if evalCode == EVAL_TOKENS, it is actually MakeCCcond1()!
         thresholds.push_back(CCNewEval(E_MARSHAL(ss << (uint8_t)EVAL_TOKENS)));	    // this is eval token cc
     if (evalcode2 != 0)
