2069741a0004b498dcf096b10281595c60a85f8e
dimxy <dimxy@komodoplatform.com>
2021-12-30 14:03:53 +0300

    duplicate py_test_util renamed

--- qa/pytest_komodo/sync/test_sync.py
+++ qa/pytest_komodo/sync/test_sync.py
@@ -5,7 +5,7 @@
 
 import pytest
 import time
-from pytest_util import env_get, get_chainstate, check_notarized, get_notary_stats
+from pytest_util_env import env_get, get_chainstate, check_notarized, get_notary_stats
 
 
 @pytest.mark.usefixtures("proxy_connection")
